18f7e45913ecad7213420594867e135314086f0e
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-18 19:29:33 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -16,3 +16,17 @@ mongo_client = MongoClient(MONGO_URL)
 mongo_db = mongo_client["cloned_vjbotz"]
 mongo_collection = mongo_db[DB_NAME]
 
+async def restart_bots():
+    logging.info("Restarting all bots........")
+    bots = list(mongo_db.bots.find())
+    for bot in bots:
+        bot_token = bot['token']
+        try:
+            ai = Client(
+                f"{bot_token}", API_ID, API_HASH,
+                bot_token=bot_token,
+                plugins={"root": "clone_plugins"},
+            )
+            await ai.start()
+        except Exception as e:
+            logging.exception(f"Error while restarting bot with token {bot_token}: {e}")
